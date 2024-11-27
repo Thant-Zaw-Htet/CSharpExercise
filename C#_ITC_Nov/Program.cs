987bdde3e53da6c4d1c7575be1b29667b5c686eb
Thant Zaw Htet <Thant.Zaw.Htet@fusionsol.com>
2024-11-27 21:48:44 +0630

    myself

--- C#_ITC_Nov/Program.cs
+++ C#_ITC_Nov/Program.cs
@@ -10,6 +10,20 @@ namespace C__ITC_Nov
     {
         static void Main(string[] args)
         {
+            string name = "Ko Zaw Htet";
+            string address = "Yangon";
+            int age = 27;
+            string education = "Bachelor of Computer Science";
+            string university = "Yangon";
+            int year = 2018;
+
+            string ambition = "Backend Developer";
+
+            string myself = $"My name is {name} and I'm from {address}. I am {age} years old and graduated with {education} from \n{university} since {year}. I want to be a professional {ambition} because I am passionate about creating efficient,\nscalable systems that power applications and solve real-world problems. I enjoy working with data, optimizing \nperformance, and ensuring that users can interact with applications seamlessly. The challenge of designing robust \nback-end systems and building the infrastructure behind web applications excites me. Additionally, I find it rewarding \nto work on technologies that help applications run smoothly, securely, and at scale.";
+
+            Console.WriteLine(myself);
+
+
 
 
         }
