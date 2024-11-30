5bfa74e6c53a65e337504d1f9575814944b2e543
Thant Zaw Htet <Thant.Zaw.Htet@fusionsol.com>
2024-11-30 19:47:06 +0630

    BloodType

---
+++ BloodType/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodType
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Select father blood type (A, B, AB, O): ");
+            string fatherBT = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Select mother blood type (A, B, AB, O): ");
+            string motherBT = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("The child blood type will be:");
+
+            switch (fatherBT)
+            {
+                case "A":
+                    switch (motherBT)
+                    {
+                        case "A":
+                            Console.WriteLine("A, O");
+                            break;
+                        case "B":
+                            Console.WriteLine("A, B, AB, O");
+                            break;
+                        case "AB":
+                            Console.WriteLine("A, B, AB");
+                            break;
+                        case "O":
+                            Console.WriteLine("A, O");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid mother blood type.");
+                            break;
+                    }
+                    break;
+
+                case "B":
+                    switch (motherBT)
+                    {
+                        case "A":
+                            Console.WriteLine("A, B, AB, O");
+                            break;
+                        case "B":
+                            Console.WriteLine("B, O");
+                            break;
+                        case "AB":
+                            Console.WriteLine("A, B, AB");
+                            break;
+                        case "O":
+                            Console.WriteLine("B, O");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid mother blood type.");
+                            break;
+                    }
+                    break;
+
+                case "AB":
+                    switch (motherBT)
+                    {
+                        case "A":
+                            Console.WriteLine("A, B, AB");
+                            break;
+                        case "B":
+                            Console.WriteLine("A, B, AB");
+                            break;
+                        case "AB":
+                            Console.WriteLine("A, B, AB");
+                            break;
+                        case "O":
+                            Console.WriteLine("A, B");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid mother blood type.");
+                            break;
+                    }
+                    break;
+
+                case "O":
+                    switch (motherBT)
+                    {
+                        case "A":
+                            Console.WriteLine("A, O");
+                            break;
+                        case "B":
+                            Console.WriteLine("B, O");
+                            break;
+                        case "AB":
+                            Console.WriteLine("A, B");
+                            break;
+                        case "O":
+                            Console.WriteLine("O");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid mother blood type.");
+                            break;
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid father blood type.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter your nationality: ");
+            //string nat = Console.ReadLine();
+
+            //Console.WriteLine("Enter your age: ");
+
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //if (nat.Equals("Myanmar") && age == 18)
+            //{
+            //    Console.WriteLine("You can vote.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You can't vote.");
+            //}
+
+            //Console.WriteLine("Enter your mark: ");
+            //int mark = int.Parse(Console.ReadLine());
+
+            //if (mark >= 40 && mark <= 79)
+            //{
+            //    Console.WriteLine("You pass exam");
+            //}
+            //else if (mark > 79)
+            //{
+            //    Console.WriteLine("You pass exam with D");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("You failed");
+            //}
+
+
+            //Console.WriteLine("Enter a card number: ");
+            //int cardNumber = int.Parse(Console.ReadLine());
+            //switch (cardNumber)
+            //{
+            //    case 11:
+            //        Console.WriteLine("Jack");break;
+            //    case 12:
+            //        Console.WriteLine("Queen"); break;
+            //    case 13:
+            //        Console.WriteLine("King");break;
+            //    default:
+            //        Console.WriteLine("Plain Card");break;
+            //}
+
+       
+
+
+
+
+
+        }
+    }
+}
